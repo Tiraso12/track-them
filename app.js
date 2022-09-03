393232219636bb7e6b24bd5c802489b239a88784
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-09-03 12:07:08 -0400

    first push , seudo coding!

---
+++ app.js
@@ -0,0 +1,17 @@
+//SEUDO
+
+//PROMP WITH OPTIONS FOR : ALL DEPARTMENTS, ALL ROLES, ALL EMPLOYEES,  ADD DEPARTMENT, ADD ROLE, ADD EMPLOYEE, AND UPDATE AN EMPLOYEE ROLE
+
+//ALL DEPARTMENTS : department names, and department ids
+
+//ALL ROLES: job title, role id, the department that role belongs to, and the salary for that role.
+
+//ALL EMPLOYEES: employee ids, first names, last names, job titles, departments, salaries, and managers that the emplyees report to.
+
+//ADD DEPARTMENT: prompt to enter name of department, then department is added to db.
+
+//ADD ROLE: prompt to enter name, salary, and deparment for the role and that role is added to db,
+
+//ADD EMPLOYEE: prompt to enter first name, last name, role, and manager, that employee is added to db
+
+//UPDATE AN EMPLOYEE ROLE: prompt to select an employee to update their new role and this information is updated in the db
