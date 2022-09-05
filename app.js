7b5fae95b67cdde4b7fd07c35b5b17ba56297c34
Gonzalo <gonzalo14_2@hotmail.com>
2022-09-05 15:15:42 -0400

    added functions to display tables

--- app.js
+++ app.js
@@ -34,25 +34,62 @@ const start = answers => {
             type: 'list',
             name: 'options',
             message: 'select an option',
-            choices: ['ALL DEPARTMENTS', 'ALL ROLES', 'ALL EMPLOYESS', 'ADD DEPARTMENT', 'ADD ROLE', 'ADD EMPLOYEE', 'UPDATE EMPLOYEE ROLE']
+            choices: ['ALL DEPARTMENTS', 'ALL ROLES', 'ALL EMPLOYEES', 'ADD DEPARTMENT', 'ADD ROLE', 'ADD EMPLOYEE', 'UPDATE EMPLOYEE ROLE']
         },
     ])
         .then(answers => {
             console.log(answers.options);
             switch (answers.options) {
                 case "ALL DEPARTMENTS":
-                    db.query(`SELECT * FROM department`, (err, res)=>{
-                        if (err) throw err;
-                        console.table(res);
-                    })
+                    allDepartments();
                     break;
                 case "ALL ROLES":
-                    console.log('Options is all roles');
+                    allRoles();
+                    break;
+                case 'ALL EMPLOYEES':
+                    allEmployees();
+                    break;
+                case 'ADD DEPARTMENT':
+                    addDeparment();
+                    break;
+                case 'ADD ROLE':
+                    addRole();
+                    break;
+                case 'ADD EMPLOYEE':
+                    addEmployee();
+                    break;
+                case 'UPDATE EMPLOYEE ROLE':
+                    updateEmployee();
                     break;
 
         }
         })
 };
+
+function allDepartments (){
+    db.query(`SELECT * FROM department`, (err, res)=>{
+        if (err) throw err;
+        console.table(res);
+    });
+    start();
+};
+
+function allRoles(){
+    db.query(`SELECT * FROM roles`,(err,res)=>{
+        if(err) throw err;
+        console.table(res);
+    })
+    start();
+};
+
+function allEmployees() {
+    db.query(`SELECT * FROM employee`, (err,res)=>{
+        if(err) throw err;
+        console.log('====================');
+        console.table(res)
+    });
+    start();
+}
 
 
 start()
--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host:'localhost',
-    user:'',
-    password:'',
-    database:''
+    user:'root',
+    password:'Slowride@22!',
+    database:'employee_tr'
 });
 
 module.exports = db;
