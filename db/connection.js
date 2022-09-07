432b92cf45420997dd9ca66596b47a4b4b68c69c
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-09-07 15:44:38 -0400

    addRole function working

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'Majorablack1995!?',
+    password:'',
     database:'employee_tr'
 });
 
